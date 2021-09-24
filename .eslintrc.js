9e70076d0d3483daef0aa0e2c536c439322758ca
XiaoLuo01 <1428620591@qq.com>
2021-09-25 00:23:57 +0800

    chore: add prettier and eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // 指定ESLint解析器
+  extends: [
+    'plugin:react/recommended', // 使用来自 @eslint-plugin-react 的推荐规则
+    'plugin:@typescript-eslint/recommended', // 使用来自@typescript-eslint/eslint-plugin的推荐规则
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2018, // 允许解析最新的 ECMAScript 特性
+    sourceType: 'module', // 允许使用 import
+    ecmaFeatures: {
+      jsx: true, // 允许对JSX进行解析
+    },
+  },
+  rules: {
+    // 自定义规则
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'linebreak-style': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'no-console': 1,
+    //关闭使用解构赋值的检测
+    // 'react/destructuring-assignment': [0, 'always'],
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect', // 告诉 eslint-plugin-react 自动检测 React 的版本
+    },
+  },
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+interface indexProps {}
+
+const index: React.FC<indexProps> = (props) => {
+  console.log('111'); //sy-log
+
+  // useEffect(() => {}, []);
+  return <div></div>;
+};
+
+export default index;
