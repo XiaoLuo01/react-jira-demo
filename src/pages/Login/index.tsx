4cb506bf5c4c7f96a81f837c1a831a1d3ae06906
XiaoLuo01 <1428620591@qq.com>
2021-10-05 00:04:16 +0800

    feat: add useAuth context  hook

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from 'context/auth-context';
 import React, { FormEvent } from 'react';
 
 interface LoginProps {}
@@ -5,18 +6,7 @@ interface LoginProps {}
 const apiURL = process.env.REACT_APP_API_URL;
 
 const Login: React.FC<LoginProps> = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiURL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(param),
-    }).then(async response => {
-      if (response.ok) {
-      }
-    });
-  };
+  const { login, user } = useAuth();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -27,6 +17,7 @@ const Login: React.FC<LoginProps> = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {user ? <div>登录成功{user.name}</div> : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id={'username'} />
