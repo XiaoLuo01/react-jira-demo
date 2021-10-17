673cf96d5fdcb0e758ce35e0950f5f64a3d6d314
XiaoLuo01 <1428620591@qq.com>
2021-10-17 23:56:14 +0800

    feat: add row layout

--- src/authenticatedApp/index.tsx
+++ src/authenticatedApp/index.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+import { Row } from 'components/lib';
 import { useAuth } from 'context/auth-context';
 import ProjectList from 'pages/ProjectList';
 import React from 'react';
@@ -7,11 +9,40 @@ interface AuthenticatedAppProps {}
 const AuthenticatedApp: React.FC<AuthenticatedAppProps> = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectList />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectList />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main``;
 
 export default AuthenticatedApp;
